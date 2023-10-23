d5aa5c3efda0ae0e2062ccc877d8dc48b3a3aaf6
Atilio <atilio.almeida.costa@gmail.com>
2023-10-22 23:22:25 -0300

    Atualização de estilos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import List from "./components/List";
 import axios from "axios";
 import { baseURL } from "./utils/constant";
+import {BiLogoLinkedin, BiLogoGithub} from "react-icons/bi";
+import {FaYoutubeSquare} from "react-icons/fa";
+
 const App = ()=>{
   const [input,setInput] = useState("");
   const [task,setTask] = useState([]);
@@ -35,24 +38,125 @@ const App = ()=>{
     });
     
   }
-
-  return <main>
-    <h1 className="tittle">Operações de Crud</h1>
+  const redirectVideo=(tipo)=>{
+    if(tipo ==="Original")
+    window.open('https://youtu.be/xElA9mGi-I0?si=aisdx0fLtEJWOeMc', "_blank", "noreferrer");
+    else
+    window.open('https://youtu.be/xElA9mGi-I0?si=aisdx0fLtEJWOeMc', "_blank", "noreferrer");
+  }
+  const redirectLinkedin=(nome)=>{
+    switch (nome){
+      case 'Atilio':
+        window.open('https://www.linkedin.com/in/atilio-almeida-costa/', "_blank", "noreferrer");
+        break;
+      case 'João':
+        window.open('https://www.linkedin.com/in/', "_blank", "noreferrer");
+        break;
+      case 'Julia':
+        window.open('https://www.linkedin.com/in/', "_blank", "noreferrer");
+        break;
+      case 'Leandro':
+        window.open('https://www.linkedin.com/in/', "_blank", "noreferrer");
+        break;
+      case 'Pedro':
+        window.open('https://www.linkedin.com/in/', "_blank", "noreferrer");
+        break;
+      default:
+        window.open('https://www.linkedin.com/in/', "_blank", "noreferrer");
+    }
+  }
+  const redirectGithub=(nome)=>{
+    switch (nome){
+      case 'Atilio':
+        window.open('https://github.com/AtilioAlmeidaCosta', "_blank", "noreferrer");  
+        break;
+      case 'João':
+        window.open('https://github.com/', "_blank", "noreferrer");    
+        break;
+      case 'Julia':
+        window.open('https://github.com/', "_blank", "noreferrer");    
+        break;
+      case 'Leandro':
+        window.open('https://github.com/', "_blank", "noreferrer");    
+        break;
+      case 'Pedro':
+        window.open('https://github.com/', "_blank", "noreferrer");    
+        break;
+      case 'Frontend':
+        window.open('https://github.com/', "_blank", "noreferrer");    
+        break;
+      case 'Backend':
+        window.open('https://github.com/', "_blank", "noreferrer");    
+        break
+      default:
+        window.open('https://github.com/', "_blank", "noreferrer");    
+    }
+  }
+    return <main>
+    <section className="crud">
+    <h1 className="title">Operações de Crud</h1>
     <div className="input_holder">
     <input type="text" value={input} onChange={(e)=>setInput(e.target.value)} />
     <button type="submit" onClick={updateId ? updateTask: addTask}>{updateId?"Atualizar Tarefa" :"Adicionar Tarefa"}</button>
     </div>
-    <ul>
-      {task.map(task => (
-      <List key={task._id}
-            id={task._id}
-            task={task.task}
-            setUpdateUI={setUpdateUI}
-            updateMode={updateMode}
-      />
-      ))}
-      
-    </ul>
+    <div className="table">
+      <List 
+      task={task}
+      setUpdateUI={setUpdateUI}
+      updateMode={updateMode}
+      /> 
+    </div>  
+    </section>
+    <section className="content">
+      <div className="guide">
+      <h2 className="title">Passo a Passo</h2>
+      <ul>
+        <li>Vídeo de referênia original:
+        <FaYoutubeSquare className="video-icon" onClick={()=>redirectVideo('Original')}/>
+        </li>
+        <li>Vídeo explicativo do nosso projeto:
+        <FaYoutubeSquare className="video-icon" onClick={()=>redirectVideo('Nosso')}/>
+        </li>
+        <li>Repositórios
+          <ul>
+            <li>Frontend: <BiLogoGithub className="profile-icon"  onClick={()=>redirectGithub('Frontend')}></BiLogoGithub></li>
+            <li>Backend:<BiLogoGithub className="profile-icon"  onClick={()=>redirectGithub('Backend')}></BiLogoGithub></li>
+          </ul>
+        </li>
+      </ul>
+      </div>
+    
+    <div className="profiles">
+      <h2 className="title">Autores</h2>
+      <div className="autores">
+      <div className="autor">
+        <h3>Atilio</h3>
+      <BiLogoLinkedin className="profile-icon"  onClick={()=>redirectLinkedin('Atilio')}></BiLogoLinkedin>
+      <BiLogoGithub className="profile-icon"  onClick={()=>redirectGithub('Atilio')}></BiLogoGithub>
+      </div>
+      <div className="autor">
+        <h3>João</h3>
+      <BiLogoLinkedin className="profile-icon"  onClick={()=>redirectLinkedin('Atilio')}></BiLogoLinkedin>
+      <BiLogoGithub className="profile-icon"  onClick={()=>redirectGithub('Atilio')}></BiLogoGithub>
+      </div>
+      <div className="autor">
+        <h3>Julia</h3>
+      <BiLogoLinkedin className="profile-icon"  onClick={()=>redirectLinkedin('Atilio')}></BiLogoLinkedin>
+      <BiLogoGithub className="profile-icon"  onClick={()=>redirectGithub('Atilio')}></BiLogoGithub>
+      </div>
+      <div className="autor">
+        <h3>Leandro</h3>
+      <BiLogoLinkedin className="profile-icon"  onClick={()=>redirectLinkedin('Atilio')}></BiLogoLinkedin>
+      <BiLogoGithub className="profile-icon"  onClick={()=>redirectGithub('Atilio')}></BiLogoGithub>
+      </div>
+      <div className="autor">
+        <h3>Pedro</h3>
+      <BiLogoLinkedin className="profile-icon"  onClick={()=>redirectLinkedin('Atilio')}></BiLogoLinkedin>
+      <BiLogoGithub className="profile-icon"  onClick={()=>redirectGithub('Atilio')}></BiLogoGithub>
+      </div>
+      </div>
+    </div>
+    </section>
   </main>
 }
 export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState} from 'react';
 import {BsTrash} from "react-icons/bs";
 import {BiEditAlt} from "react-icons/bi";
 import axios from "axios";
 import { baseURL } from "../utils/constant";
-const List = ({id,task, setUpdateUI, updateMode}) =>{
-    const removeTask= ()=>{
+import 'react-data-grid/lib/styles.css';
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+import ReactDataGrid from 'react-data-grid';
+
+const List = ({task, setUpdateUI, updateMode}) =>{
+    const removeTask= (id)=>{
         axios.delete(`${baseURL}delete/${id}`).then((res)=>{
-         setUpdateUI((prevState)=>!prevState);   
+            setUpdateUI((prevState)=>!prevState);   
         });
     }
-    return <li>
-        {task}
-        <div className="icon_holder">
-            <BiEditAlt className="icon" onClick={()=>updateMode(id,task)}/>
-            <BsTrash className="icon" onClick={removeTask} />
-        </div>
-        </li>
-    
+    let [rows]= useState(()=>{
+        let aux=[]
+        task.map(task =>(
+            aux.push({key:task._id, id: task._id, task: task.task, 
+                edita:<BiEditAlt className="icon" onClick={()=>updateMode(task._id,task.task)}/>,
+                deleta:<BsTrash className="icon" onClick={removeTask(task._id)} /> })
+            )
+        )
+        return aux;
+        }
+    )
+    const columns = [
+        { key: "id" ,name:'ID'},
+        { key: "task" ,name:'Tarefa'},
+        { key: "edita" ,name:'Editar'},
+        { key: "deleta",name:'Deletar'}
+      ];
+    return <ReactDataGrid rows={rows} columns={columns}/>
 };
 
 export default List;
